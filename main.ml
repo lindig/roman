63e78a685615a2d8db4216c459b09975b748e7e5
Christian Lindig <lindig@gmail.com>
2015-05-03 08:56:57 +0200

    Added: tests, option -test

--- main.ml
+++ main.ml
@@ -1,16 +1,37 @@
 (** Simple command line tool to convert roman numerals to integers 
 *)
 
+let tests =     
+    [ "i"       ,1
+    ; "iv"      ,4
+    ; "II"      ,2
+    ; "mdc"     ,1600
+    ; "cmxlix"  ,949
+    ; "ix"      ,9
+    ; "xxix"    ,29
+    ; "lxvi"    ,66
+    ]
+
+let test () = 
+    List.for_all (fun (left,right) -> Roman.to_int left = right) tests
+
 (** [main] function - handles command line arguments and exit codes *)
 let main () =
    let argv = Array.to_list Sys.argv in
    let args = List.tl argv in
    let this = List.hd argv |> Filename.basename in
    match args with
-   | [str]  -> Printf.printf "%s = %d\n" str @@ Roman.to_int str; exit 0
-   | _      -> Printf.eprintf "usage: %s <roman numeral>\n" this; exit 1
+   | ["-test"] -> if test () 
+                  then (Printf.printf "passed\n"; exit 0)
+                  else (Printf.printf "failed\n"; exit 1)
+   | [str]     -> Printf.printf "%d\n" @@ Roman.to_int str; exit 0
+   | _         -> Printf.eprintf "usage: %s <roman numeral>\n" this; exit 1
+
+
 
         
-let () = main ()
+let () = 
+    try main () with 
+    | Roman.Error msg -> Printf.eprintf "%s\n" msg; exit 1
 
 
--- roman.mli
+++ roman.mli
@@ -1,9 +1,12 @@
 
+(** raised in case of error *)
+exception Error of string
+
 (** [to_int str] takes a roman numeral [str] and computes its value 
     as an integer. [to_int] is case insensitive and accepts the modern 
     syntax for roman numerals as described in 
     http://en.wikipedia.org/wiki/Roman_numerals *)
 
-val to_int: string -> int
+val to_int: string -> int (* raises Error *)
 
 
